aadd25a5299f99b0a113ca77c17db5a17298bf53
Qiaoyi-Gao <62561597+Qiaoyi-Gao@users.noreply.github.com>
2020-03-25 12:53:24 +0100

    setting up a Spring boot project environment
    Payment processing to be started by setting up a Spring boot project environment able to support REST calls.

---
+++ payments-infra/src/main/java/com/acn/payments/PaymentsInfraApplication.java
@@ -0,0 +1,13 @@
+package com.acn.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsInfraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsInfraApplication.class, args);
+	}
+
+}
---
+++ payments-infra/src/test/java/com/acn/payments/PaymentsInfraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acn.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsInfraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
